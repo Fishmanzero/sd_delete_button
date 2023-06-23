84b94324b6ca2d9ae9142006aa24852830dc07c3
reforget-id <58993868+reforget-id@users.noreply.github.com>
2023-06-24 00:12:58 +0700

    Update sd_delete_button.js
    Extras tab

--- javascript/sd_delete_button.js
+++ javascript/sd_delete_button.js
@@ -8,6 +8,11 @@ function img2img_sdelb_hide() {
     img2img_sdelb_delete_info.style.display = 'none'
 }
 
+function extras_sdelb_hide() {
+    const img2img_sdelb_delete_info = gradioApp().getElementById('extras_sdelb_delete_info')
+    extras_sdelb_delete_info.style.display = 'none'
+}
+
 function txt2img_sdelb_addEventListener() {
     const txt2img_generate = gradioApp().getElementById('txt2img_generate')
     txt2img_generate.removeEventListener('click', txt2img_sdelb_hide)
@@ -22,4 +27,12 @@ function img2img_sdelb_addEventListener() {
     img2img_generate.addEventListener('click', img2img_sdelb_hide)
     const img2img_sdelb_delete_info = gradioApp().getElementById('img2img_sdelb_delete_info')
     img2img_sdelb_delete_info.style.display = ''
+}
+
+function extras_sdelb_addEventListener() {
+    const extras_generate = gradioApp().getElementById('extras_generate')
+    extras_generate.removeEventListener('click', extras_sdelb_hide)
+    extras_generate.addEventListener('click', extras_sdelb_hide)
+    const extras_sdelb_delete_info = gradioApp().getElementById('extras_sdelb_delete_info')
+    extras_sdelb_delete_info.style.display = ''
 }
